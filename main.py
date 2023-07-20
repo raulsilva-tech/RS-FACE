95945edfba1e1413c438092d3e0163af548bd958
raulsilva-tech <140067414+raulsilva-tech@users.noreply.github.com>
2023-07-20 21:39:43 +1000

    ..

--- main.py
+++ main.py
@@ -273,7 +273,7 @@ def app_reset_list():
 
 
 # iniciando em modo debug
-if __name__ == "__main__":
-    app.run(host='0.0.0.0',port=5000)
+# if __name__ == "__main__":
+#     app.run(host='0.0.0.0',port=5000)
 
-# serve(app, host='0.0.0.0', port=5000) #WAITRESS!
+serve(app, host='0.0.0.0', port=5000) #WAITRESS!
